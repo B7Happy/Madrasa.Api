fba1fb1a7e7a45424330618a0f28d36dcd7fd853
saly.moubarak@gmail.com <saly.moubarak@gmail.com>
2024-04-11 23:08:21 +0200

    add swagger

--- Madrasa.Api/Program.cs
+++ Madrasa.Api/Program.cs
@@ -107,14 +107,15 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+app.UseCors("AllowAnyOrigin");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseCors("AllowAnyOrigin");
+//if (app.Environment.IsDevelopment())
+//{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseAuthentication();
 app.UseAuthorization();
